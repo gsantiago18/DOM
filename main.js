7d940a901580963ecb52842df6ca9b1905c0a318
Gabriel Gonzalez <81981786+gsantiago18@users.noreply.github.com>
2024-06-24 13:37:19 -0500

    Creando Listas Dinamicas con JS

---
+++ main.js
@@ -0,0 +1,51 @@
+const title = document.getElementById('title');
+console.log(title);
+
+const mainDiv = document.querySelector(".main-div");
+console.log(mainDiv)
+
+let list = document.querySelectorAll("ul.List> li")
+console.log(list)
+
+title.innerText ='Gabo manipulando el DOM'
+
+title.innerHTML ="<em>Nuevo</em>";
+
+let image=document.querySelector("img")
+image.setAttribute("src", "vite.svg");
+image.classList.add("img-fluid");
+mainDiv.style.backgroundColor = "#ffcc00"
+
+let oldDiv =document.querySelector(".old-div")
+document.body.removeChild(oldDiv);
+
+let newDiv = document.createElement("div")
+newDiv.innerText='Soy un nuevo div'
+document.body.appendChild(newDiv)
+
+let button = document.querySelector("button")
+/*button.addEventListener("click", ()=>{
+  alert("Boton presionado ")
+})*/
+
+function showMessage(message){
+  alert(message)
+}
+
+button.addEventListener(
+  "click",
+   showMessage.bind(null, 'Boton presionado'))
+
+
+   let input = document.querySelector("#itemInput")
+   let addButton = document.querySelector("#addButton")
+   let lisst = document.querySelector("#dynamicList")
+
+   addButton.addEventListener("click",()=>{
+    if(input.value.trim()!=""){
+      let newItem = document.createElement("li");
+      newItem.innerText = input.value;
+      lisst.appendChild(newItem)
+      input.value="";
+    }
+   });
