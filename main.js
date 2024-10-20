4875e4ebb085717c9add4021045e38878d2ce699
Gabriel Gonzalez <81981786+gsantiago18@users.noreply.github.com>
2024-10-20 02:29:31 -0500

    Creacion de modales opacidad

--- main.js
+++ main.js
@@ -65,3 +65,63 @@ filterInput.addEventListener("keyup", function(){
     }
   })
 })
+
+const draggable = document.getElementById("draggable")
+const dropzone = document.getElementById('dropzone')
+
+draggable.addEventListener("dragstart", function(event){
+  setTimeout(()=>{
+    this.style.display="none";
+  }, 0)
+})
+
+dropzone.addEventListener("dragover" ,function(event){
+  event.preventDefault();
+})
+
+dropzone.addEventListener("drop", function(){
+  draggable.style.display="block";
+  this.append(draggable);
+})
+
+let opc = document.querySelector("#opc")
+let modalTabla =  document.getElementById("myModal");
+let span = document.getElementsByClassName("close")[0];
+opc.addEventListener("change",()=>{
+  if(opc.value!=='70'){
+   swal.fire({
+    title:'Advertencia',
+    html:'La opacidad que seleccionaste es: ' + opc.value + 
+            ' lo cual puede ocasionar sobrecostos. <a href="#" id="verTablaLink">Ver tabla</a>',
+    icon:'warning',
+    didOpen:()=>{
+        document.getElementById("verTablaLink").addEventListener('click',function(e){
+          e.preventDefault()
+
+          Swal.close()
+          modalTabla.style.display ="block"
+        })
+      }       
+    })
+   }
+
+})
+
+span.onclick =function(){
+  modalTabla.style.display="none"
+}
+
+window.onclick = function(event) {
+  if (event.target == modalTabla) {
+    modalTabla.style.display = "none";
+  }
+}
+
+document.getElementById("btnSi").addEventListener('click',function(){
+  modalTabla.style.display="none"
+})
+
+document.getElementById("btnNo").addEventListener('click',function(){
+  opc.value='70'
+  modalTabla.style.display="none"
+})
